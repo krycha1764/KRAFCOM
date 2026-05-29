824c18c6d2de6c7b9b1535c678b98b9294042dd7
krycha1764 <mikolajkrysiak.03@gmail.com>
2026-05-29 16:20:45 +0200

    init receiver function done

--- library/KRAFCOM.c
+++ library/KRAFCOM.c
@@ -1,12 +1,25 @@
 
 #include "KRAFCOM.h"
 
+#include <string.h>
+
 void KRAFCOM_InitReceiver(KRAFCOM_Receiver *receiver, const uint8_t* key, KRAFCOM_PacketHandler packetHandlerCallback, void *context) {
 
-	(void)receiver;
-	(void)key;
-	(void)packetHandlerCallback;
-	(void)context;
+	if(receiver == NULL) return;
+	if(packetHandlerCallback == NULL) return;
+
+	receiver->counter = 0;
+	receiver->context = context;
+	receiver->packetHandler = packetHandlerCallback;
+
+	receiver->receivedPacket.sop = 0;
+	receiver->receivedPacket.type = 0;
+	receiver->receivedPacket.crc32 = 0;
+	receiver->receivedPacket.time = 0;
+
+	memset(receiver->receivedPacket.iv, 0x00, KRAFCOM_IV_SIZE);
+	memset(receiver->receivedPacket.payload, 0x00, KRAFCOM_MAX_PAYLOAD);
+
 
 }
 
